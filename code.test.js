9397e8c8d9dbb27dad5b427b709b4515d737a0e5
LukeHoese <157862417+LukeHoese@users.noreply.github.com>
2024-12-10 22:04:38 -0700

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -6,4 +6,4 @@ eval(fs.readFileSync('code.js')+'');
 
 const infix1 = "(1+2)-(33*44+55)"
 const postfix1 = toPostfix(infix1);
-assert(postfix1 == "1 2 + 33 44 * 55 + -"));
+assert(postfix1 == "1 2 + 33 44 * 55 + -");
