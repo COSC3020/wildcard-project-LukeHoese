9188c7ce7c0ba780b315985c4e979944a7a36adc
LukeHoese <157862417+LukeHoese@users.noreply.github.com>
2024-12-10 22:04:10 -0700

    Create code.test.js

---
+++ code.test.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const jsc = require('jsverify');
+const assert = require('assert');
+
+eval(fs.readFileSync('code.js')+'');
+
+const infix1 = "(1+2)-(33*44+55)"
+const postfix1 = toPostfix(infix1);
+assert(postfix1 == "1 2 + 33 44 * 55 + -"));
